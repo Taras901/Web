c41cb1a558aefb879e2ec4e1ffc334d05f024d2d
Taras901 <golubetstarasq@gmail.com>
2025-09-29 22:42:38 +0300

    Add files via upload

---
+++ Web_lab_1/main.js
@@ -0,0 +1,11 @@
+const OPEN_CLASSNAME = "open";
+
+const navLinks = document.getElementById("div_id");
+
+function toggleMenu() {
+    if (div_id.classList.contains(OPEN_CLASSNAME)) {
+    div_id.classList.remove(OPEN_CLASSNAME);
+    } else {
+    div_id.classList.add(OPEN_CLASSNAME);
+    }
+}
